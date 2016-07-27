5d8c99db3a71468e05f28cacfbad9b949c17bf6a
Adam Conder <adamdconder@gmail.com>
2016-07-27 12:14:50 +0100

    refactoring proxy

--- app/uk/gov/hmrc/brm/controllers/MicroserviceHelloWorld.scala
+++ app/uk/gov/hmrc/brm/controllers/MicroserviceHelloWorld.scala
@@ -14,24 +14,18 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.birthregistrationmatchingproxy.controllers
+package uk.gov.hmrc.brm.controllers
 
-import play.api.http.Status
-import play.api.test.FakeRequest
-import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+import uk.gov.hmrc.play.microservice.controller.BaseController
+import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext._
+import play.api.mvc._
+import scala.concurrent.Future
 
+object MicroserviceHelloWorld extends MicroserviceHelloWorld
 
-class MicroserviceHelloWorldControllerSpec extends UnitSpec with WithFakeApplication {
+trait MicroserviceHelloWorld extends BaseController {
 
-  val fakeRequest = FakeRequest("GET", "/")
-
-
-  "GET /" should {
-    "return 200" in {
-      val result = MicroserviceHelloWorld.hello()(fakeRequest)
-      status(result) shouldBe Status.OK
-    }
-  }
-
-
+	def hello() = Action.async { implicit request =>
+		Future.successful(Ok("Hello world"))
+	}
 }
