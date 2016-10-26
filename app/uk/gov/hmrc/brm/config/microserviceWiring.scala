2b8e9c5af72d2d40091ccbcd94ff27ddb96d06ac
Adam Conder <adamdconder@gmail.com>
2016-10-26 12:04:50 +0100

    remove authentication dependencies
    - remove imports
    - removing jar

--- app/uk/gov/hmrc/brm/config/microserviceWiring.scala
+++ app/uk/gov/hmrc/brm/config/microserviceWiring.scala
@@ -18,7 +18,6 @@ package uk.gov.hmrc.brm.config
 
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
-import uk.gov.hmrc.play.auth.microservice.connectors.AuthConnector
 import uk.gov.hmrc.play.config.{AppName, RunMode, ServicesConfig}
 import uk.gov.hmrc.play.http.hooks.HttpHook
 import uk.gov.hmrc.play.http.ws._
