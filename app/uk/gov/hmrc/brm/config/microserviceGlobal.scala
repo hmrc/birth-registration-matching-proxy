87c17897e5d89a781c7c31a99ec98f30369bd113
Manish Wadhwani <manish.wadhwani@digital.hmrc.gov.uk>
2016-12-08 17:10:21 +0000

    BRMS-151-Datastream: updated to not log body in audit logs.

--- app/uk/gov/hmrc/brm/config/microserviceGlobal.scala
+++ app/uk/gov/hmrc/brm/config/microserviceGlobal.scala
@@ -46,6 +46,9 @@ object GROConnectorConfiguration extends ServicesConfig {
   lazy val readTimeout = getConfInt("birth-registration-matching.readTimeout", throw new RuntimeException("[Configuration][NotFound] birth-registration-matching.readTimeout"))
   lazy val delayAttemptInMilliseconds : DelayTime = getConfInt("birth-registration-matching.delayAttemptInMilliseconds", throw new RuntimeException("[Configuration][NotFound] birth-registration-matching.delayAttemptInMilliseconds"))
   lazy val delayAttempts : DelayAttempts = getConfInt("birth-registration-matching.delayAttempts", throw new RuntimeException("[Configuration][NotFound] birth-registration-matching.delayAttempts"))
+  lazy val  blockedBodyWords =   Play.current.configuration.getStringSeq(s"$rootServices.birth-registration-matching.no-audit-word-list")
+  println("")
+  println("foundwords...... "+blockedBodyWords)
 }
 
 object MicroserviceAuditFilter extends AuditFilter with AppName {
--- app/uk/gov/hmrc/brm/config/microserviceWiring.scala
+++ app/uk/gov/hmrc/brm/config/microserviceWiring.scala
@@ -16,16 +16,76 @@
 
 package uk.gov.hmrc.brm.config
 
+import play.api.libs.json.JsValue
+import uk.gov.hmrc.brm.utils.BrmLogger._
 import uk.gov.hmrc.play.audit.http.config.LoadAuditingConfig
-import uk.gov.hmrc.play.audit.http.connector.AuditConnector
+import uk.gov.hmrc.play.audit.http.connector.AuditEventFailureKeys._
+import uk.gov.hmrc.play.audit.http.connector.{AuditConnector, AuditResult, LoggerProvider}
 import uk.gov.hmrc.play.config.{AppName, RunMode}
+import uk.gov.hmrc.play.http.HttpResponse
 import uk.gov.hmrc.play.http.hooks.HttpHook
+import uk.gov.hmrc.play.http.logging.LoggingDetails
 import uk.gov.hmrc.play.http.ws._
+import scala.util.control.Breaks._
+
+import scala.concurrent.Future
 
 object WSHttp extends WSGet with WSPut with WSPost with WSDelete with WSPatch with AppName {
   override val hooks: Seq[HttpHook] = NoneRequired
 }
 
-object MicroserviceAuditConnector extends AuditConnector with RunMode {
+object MicroserviceAuditConnector extends AuditConnector with RunMode with BRMResultHandler {
   override lazy val auditingConfig = LoadAuditingConfig(s"auditing")
+}
+
+trait BRMResultHandler extends uk.gov.hmrc.play.audit.http.connector.ResultHandler {
+
+  this: LoggerProvider =>
+  import scala.concurrent.ExecutionContext.Implicits.global
+  override protected def handleResult(resultF: Future[HttpResponse], body: JsValue)(implicit ld: LoggingDetails): Future[HttpResponse] = {
+    resultF
+      .recoverWith {
+        case t =>
+          println("body0......... " )
+          var message = ""
+          if (isContainBlockedWord(body)) {
+            message = makeFailureMessageWithoutBody(body)
+
+          } else {
+            message = makeFailureMessage(body)
+          }
+          println("body01......... " + message)
+        logError(message, t)
+        Future.failed(AuditResult.Failure(message, Some(t)))
+      }
+      .map { response =>
+        checkResponse(body, response) match {
+          case Some(error) =>
+            println("body1......... " + error)
+            logError(error)
+            throw AuditResult.Failure(error)
+          case None => response
+        }
+      }
+  }
+
+  private def isContainBlockedWord(body: JsValue) : Boolean = {
+    var  returnValue: Boolean = false
+    var stringValue = body.toString()
+   // val noAuditWordList = Seq("child", "subjects", "givenname")
+    val noAuditWordList =  GROConnectorConfiguration.blockedBodyWords.get
+    breakable {
+      for (notAllowedworld <- noAuditWordList) {
+        var isContains = stringValue.toLowerCase.contains(notAllowedworld.toLowerCase())
+        if (isContains) {
+          returnValue = true
+          scala.util.control.Breaks.break();
+        }
+      }
+    }
+    info("BRMResultHandler", "isContainBlockedWord",s" isContainBlockedWord  ${returnValue}")
+    returnValue
+  }
+
+ protected def makeFailureMessageWithoutBody(body: JsValue): String = s"$LoggingAuditRequestFailureKey :"
 }
