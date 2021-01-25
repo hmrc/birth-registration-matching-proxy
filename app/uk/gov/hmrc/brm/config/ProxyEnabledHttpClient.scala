8474635d20ff9c3789ceb6aedee0e0cb3408ee59
RafalSobol <52891748+RafSobol@users.noreply.github.com>
2021-01-25 17:23:08 +0000

    [RS][DDCE-1288] Fixed requests sent via proxy.

---
+++ app/uk/gov/hmrc/brm/config/ProxyEnabledHttpClient.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.brm.config
+
+import akka.actor.ActorSystem
+import com.typesafe.config.Config
+import play.api.Configuration
+import play.api.libs.ws.{WSClient, WSProxyServer}
+import uk.gov.hmrc.http.hooks.HttpHook
+import uk.gov.hmrc.play.audit.http.HttpAuditing
+import uk.gov.hmrc.play.bootstrap.http.HttpClient
+import uk.gov.hmrc.play.http.ws.{WSHttp, WSProxy, WSProxyConfiguration}
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class ProxyEnabledHttpClient @Inject()(
+                                   config: Configuration,
+                                   val httpAuditing: HttpAuditing,
+                                   override val wsClient: WSClient,
+                                   override protected val actorSystem: ActorSystem)
+  extends HttpClient with WSHttp with WSProxy {
+
+  override lazy val configuration: Option[Config] = Option(config.underlying)
+
+  override val hooks: Seq[HttpHook] = Seq(httpAuditing.AuditingHook)
+
+  override def wsProxyServer: Option[WSProxyServer] = WSProxyConfiguration("microservice.services.proxy", config)
+}
