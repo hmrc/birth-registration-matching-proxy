5c9d5e7d070cdf3f650cdecd15472dd4c0b49c75
Daniel Pennington <63240616+DannyPennington97@users.noreply.github.com>
2024-07-03 13:15:49 +0100

    [DDCE-5592] Remove old keystore config

--- app/uk/gov/hmrc/brm/config/CustomWSConfigParser.scala
+++ app/uk/gov/hmrc/brm/config/CustomWSConfigParser.scala
@@ -20,11 +20,7 @@ import com.typesafe.sslconfig.ssl.KeyStoreConfig
 import play.api.inject.{Binding, Module}
 import play.api.libs.ws.{WSClientConfig, WSConfigParser}
 import play.api.{Configuration, Environment}
-import uk.gov.hmrc.brm.utils.BrmLogger
 
-import java.io.{File, FileOutputStream}
-import java.nio.charset.StandardCharsets
-import java.util.Base64
 import javax.inject.{Inject, Singleton}
 
 @Singleton
@@ -39,16 +35,7 @@ class CustomWSConfigParser @Inject() (configuration: Configuration, env: Environ
     val config         = internalParser.parse()
 
     val keyStoreConfig: scala.collection.immutable.Seq[KeyStoreConfig] =
-      config.ssl.keyManagerConfig.keyStoreConfigs.filter(_.data.forall(_.nonEmpty)).map { ks =>
-        (ks.storeType.toUpperCase, ks.filePath, ks.data) match {
-          case (_, None, Some(data)) =>
-            createKeyStoreConfig(ks, data)
-
-          case other =>
-            BrmLogger.info(className, "parse", s"Adding ${other._1} type keystore")
-            ks
-        }
-      }
+      config.ssl.keyManagerConfig.keyStoreConfigs
 
     val wsClientConfig = config.copy(
       ssl = config.ssl
@@ -59,35 +46,6 @@ class CustomWSConfigParser @Inject() (configuration: Configuration, env: Environ
     )
 
     wsClientConfig
-  }
-
-  //TODO remove this below and clean up above after DDCE-5592 5593 and 5594 are complete. Also clean up configs
-  /**
-    * @return absolute file path with the bytes written to the file
-    */
-  def createTempFileForData(data: String): (String, Array[Byte]) = {
-    val file = File.createTempFile(getClass.getSimpleName, ".tmp")
-    file.deleteOnExit()
-    val os   = new FileOutputStream(file)
-    try {
-      val bytes = Base64.getDecoder.decode(data.getBytes(StandardCharsets.US_ASCII))
-      os.write(bytes)
-      os.flush()
-      file.getAbsolutePath -> bytes
-    } finally os.close()
-  }
-
-  private def createKeyStoreConfig(ks: KeyStoreConfig, data: String): KeyStoreConfig = {
-    BrmLogger.info(className, "createKeyStoreConfig", "Creating key store config")
-    val (ksFilePath, _) = createTempFileForData(data)
-    BrmLogger.info(className, "createKeyStoreConfig", s"Successfully wrote keystore data to file: $ksFilePath")
-
-    val decryptedPass = ks.password
-      .filter(_.nonEmpty)
-      .map(pass => Base64.getDecoder.decode(pass.getBytes(StandardCharsets.US_ASCII)))
-      .map(new String(_, StandardCharsets.US_ASCII))
-
-    ks.withFilePath(Some(ksFilePath)).withPassword(decryptedPass)
   }
 
 }
