453fbdf7d89bfc1c0c049640dd81ff2a84fcf442
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2025-05-15 14:44:12 +0100

    GET-CERT-DATE-EXAMPLE get certificate date example

--- app/uk/gov/hmrc/brm/config/CustomWSConfigParser.scala
+++ app/uk/gov/hmrc/brm/config/CustomWSConfigParser.scala
@@ -21,13 +21,60 @@ import play.api.inject.{Binding, Module}
 import play.api.libs.ws.{WSClientConfig, WSConfigParser}
 import play.api.{Configuration, Environment}
 
+import java.io.FileInputStream
+import java.security.KeyStore
+import java.security.cert.X509Certificate
 import javax.inject.{Inject, Singleton}
+import scala.jdk.CollectionConverters.EnumerationHasAsScala
+import scala.util.{Failure, Success, Try}
 
 @Singleton
 class CustomWSConfigParser @Inject() (configuration: Configuration, env: Environment)
     extends WSConfigParser(configuration.underlying, env.classLoader) {
 
   lazy val className: String = this.getClass.getSimpleName
+
+  def extractCertificateDates(
+    keystorePath: String = "conf/cert/certificate.p12",
+    keystorePassword: String = "cGFzc3dvcmQ="
+  ): Try[(String, java.util.Date, java.util.Date)] =
+    Try {
+
+      val keystore    = KeyStore.getInstance("PKCS12")
+      val inputStream = new FileInputStream(keystorePath)
+
+      try {
+        keystore.load(inputStream, keystorePassword.toCharArray)
+
+        val aliases    = keystore.aliases.asScala.toList
+        val firstAlias = aliases.head
+
+        keystore.getCertificate(firstAlias) match {
+          case certificate: X509Certificate => (firstAlias, certificate.getNotBefore, certificate.getNotAfter)
+          case _                            => throw new Exception("Error getting certificate")
+        }
+
+      } finally inputStream.close()
+    }
+
+  // Usage example
+  def printCertificateDates(
+    keystorePath: String = "conf/cert/certificate.p12",
+    keystorePassword: String = "cGFzc3dvcmQ="
+  ): Unit =
+    extractCertificateDates(keystorePath, keystorePassword) match {
+      case Success((alias, notBefore, notAfter)) =>
+        println("====================== CERTIFICATE DETAILS ======================")
+        println()
+        println(s"Certificate alias: $alias")
+        println(s"Valid from: $notBefore")
+        println(s"Expires on: $notAfter")
+        println()
+        println("====================== CERTIFICATE DETAILS ======================")
+
+      case Failure(ex) =>
+        println(s"Error extracting certificate dates: ${ex.getMessage}")
+    }
 
   override def parse(): WSClientConfig = {
 
@@ -44,6 +91,10 @@ class CustomWSConfigParser @Inject() (configuration: Configuration, env: Environ
             .withKeyStoreConfigs(keyStoreConfig)
         )
     )
+
+    println()
+    printCertificateDates()
+    println()
 
     wsClientConfig
   }
